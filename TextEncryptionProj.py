bac57fb058c3f0244f79bfe58a52d72f2761d1fb
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2024-11-18 10:58:27 -0500

    "Initial commit"

---
+++ TextEncryptionProj.py
@@ -0,0 +1,12 @@
+#Programmer: Alexander Henriquez
+#Purpose of this program is to do basic text encryption
+#Data: 11/18/2024
+
+
+import random
+
+userinput = input("Type anything: ")
+
+print(userinput)
+
+
