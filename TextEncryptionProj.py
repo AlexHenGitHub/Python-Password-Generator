86c5ddee141b7276fdd8a4dd5ff0561454f622b9
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2024-11-19 14:12:53 -0500

    Creating a key for encryption plus while loop

--- TextEncryptionProj.py
+++ TextEncryptionProj.py
@@ -5,16 +5,33 @@
 
 import random
 
+from numpy.lib.user_array import container
 
-
+while True:
 
 # Get user input
-userinput = input("Type anything: ")
-if userinput:
-    key = random.randint(-10000000000000000000000000000*10000000000000000000000000000,
-                         10000000000000000000000000000 * 10000000000000000000000000000)
+    userinput = input("Type anything: ")
 
+
+
+
+    if userinput:
+        key = random.randint(-10000000000000000000000000000*10000000000000000000000000000,
+                         10000000000000000000000000000 * 10000000000000000000000000000)
     print(key)
+    again = input("Do you want to go again 1 = yes, 0 = no")
+
+
+
+    if again == "1" :
+        continue
+
+    else:
+        print("End")
+        break
+
+
+
 
 '''
 # Convert the input into a list of characters
